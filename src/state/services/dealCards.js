c9c5a981e20de3f1df9cb4ff5ffbf9dac8fb5027
emptypockets-dev <andrey.vw@gmail.com>
2023-02-21 10:21:28 -0500

    remove cards below 6

--- src/state/services/dealCards.js
+++ src/state/services/dealCards.js
@@ -18,7 +18,7 @@ const mapValueToDisplay = (value) => {
 const createDeck = () => {
   let suits = ["hearts", "diamonds", "spades", "clubs"];
   // suits = ["hearts", "diamonds", "spades", "clubs"];
-  let values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+  let values = [6, 7, 8, 9, 10, 11, 12, 13, 14];
   // values = [2, 3, 4, 5];
 
   let deck = [];
